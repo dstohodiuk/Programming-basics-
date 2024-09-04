cbc9f591991b7fa2d065c582e59adb0269d908aa
Denys Stohodiuk <128259806+dstohodiuk@users.noreply.github.com>
2024-09-04 23:47:31 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,9 @@
+my_text = "Hello, World!"
+my_name = "\nDenis"
+my_last_name = "Stohodiuk,"
+my_year = 17
+
+print(("Тип my_text:"), type(my_text), ("Тип my_name:"), type(
+    my_name), ("Тип my_last_text:"), type(my_last_name),
+    ("Тип my_year:"), type(my_year))
+print(my_text, my_name, my_last_name, my_year)
