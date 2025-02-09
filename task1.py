f6a6b302f4719424fbbd2e3217950f69f32cd2a3
Denys Stohodiuk <128259806+dstohodiuk@users.noreply.github.com>
2025-02-09 21:35:19 +0200

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,9 +1,18 @@
-my_text = "Hello, World!"
-my_name = "\nDenis"
-my_last_name = "Stohodiuk,"
-my_year = 17
+class Denys:
+    name = None
+    last_name = None
+    birth_year = None
 
-print(("Тип my_text:"), type(my_text), ("Тип my_name:"), type(
-    my_name), ("Тип my_last_text:"), type(my_last_name),
-    ("Тип my_year:"), type(my_year))
-print(my_text, my_name, my_last_name, my_year)
+    def calculates_the_course(birth_year: int):
+        my_year = 2007
+        rate = 1.0 + (birth_year - my_year) * 0.05
+        return max(rate, 0.1)
+
+    print(f"Your course: {calculates_the_course(2003):.2}")
+
+    def get(self, name, last_name):
+        self.name = 'Denys'
+        self.last_name = 'Stohodiuk'
+        result = [self.name, self.last_name]
+
+    print(result)
